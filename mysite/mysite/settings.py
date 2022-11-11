b3e63f7aa1732984606d35551ba0efafb60c0102
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2022-11-11 21:08:41 +0200

    Revert "Modified settings.py"
    This reverts commit 5e32b3bcd4de9b40dc767ad538159f7c7dff3fc3.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-h7h-q)2z)7_8e+)2n9fn4(^#o*#a$wkq78fd(@nib@a7l%=kgx'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
